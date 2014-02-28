572c217d2ca221fa6e6bdd9711bae389c04ef781
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-02-28 13:48:57 -0600

    fixed spelling typo

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,7 +2,7 @@ class Survey < ActiveRecord::Base
   belongs_to :user
   has_many :questions
   has_many :participations
-  has_many :users, through: :participants
+  has_many :users, through: :participations
 
 
 end
